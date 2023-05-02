e199dceed21dcfb9bf9cbd68f34375d8a076e0de
killthebee <superhotfire447@yandex.ru>
2023-05-02 20:57:03 +0300

    add 1-to-1 model for user profile pic

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,9 @@ RUN chmod +x /usr/src/app/entrypoint.sh
 
 # install dependencies
 RUN pip install --upgrade pip
+# apps for Pillow
+RUN apk --update add libxml2-dev libxslt-dev libffi-dev gcc musl-dev libgcc openssl-dev curl
+RUN apk add jpeg-dev zlib-dev freetype-dev lcms2-dev openjpeg-dev tiff-dev tk-dev tcl-dev
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,32 @@
+import os
 from django.db import models
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def upload_to(instance, filename):
+    """
+    Changed the output of the photo name.
+    """
+    now = timezone.now()
+    base, extension = os.path.splitext(filename.lower())
+    milliseconds = now.microsecond // 1000
+    return f"images/user/{instance.id}/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to=upload_to)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
